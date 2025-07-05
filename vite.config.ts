238b114ce8aa81c375465887c5d334af2d14162c
Jaaystones <Joelpaul345@gmail.com>
2025-07-05 01:51:37 -0700

    :wrench: Git actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,9 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-const repoName = "paul-joel-portfolio";
-export default defineConfig(({ mode }) => ({
-  base: `/${repoName}/`,
-
-  server: {
-    host: "::",
-    port: 8080, 
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+  base: "/paul-joel-portfolio/", // YOUR REPO NAME HERE
+});
